4918356d1a40faa8087a6d24bff0be61d42d862e
LetsMelon <domi.m@outlook.com>
2023-10-17 00:09:28 +0200

    replace 'copy_to' function with builtin method from rusvid_core

--- src/library/output.rs
+++ src/library/output.rs
@@ -1,39 +1,9 @@
 use anyhow::Result;
-use rusvid_core::prelude::{Pixel, Plane};
+use rusvid_core::prelude::{Pixel, Plane, ResizeMode};
 
 use crate::coordinate::Coordinate;
 use crate::input_output_value::InputOutputValue;
 use crate::node::{Node, SpaceInfo};
-
-// TODO implement this method into crate `rusvid_core`
-fn copy_plane(
-    destination: &mut Plane,
-    source: &Plane,
-    position: (u32, u32),
-    size: (u32, u32),
-) -> Result<()> {
-    let source_width = source.width() as f64 - 1.0;
-    let source_height = source.height() as f64 - 1.0;
-
-    let copy_width = size.0 as f64 - 1.0;
-    let copy_height = size.1 as f64 - 1.0;
-
-    for destination_delta_x in 0..size.0 {
-        for destination_delta_y in 0..size.1 {
-            let destination_x = position.0 + destination_delta_x;
-            let destination_y = position.1 + destination_delta_y;
-
-            let source_x = (destination_delta_x as f64 / copy_width * source_width).round() as u32;
-            let source_y =
-                (destination_delta_y as f64 / copy_height * source_height).round() as u32;
-
-            let source_pixel = source.pixel(source_x, source_y).unwrap();
-            destination.put_pixel(destination_x, destination_y, source_pixel.clone())?;
-        }
-    }
-
-    Ok(())
-}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Output {
@@ -55,15 +25,14 @@ impl Output {
         plane: &mut Plane,
         generated_plane: &Plane,
     ) -> Result<()> {
-        copy_plane(
-            plane,
+        let pos = self.space_info.position;
+        plane.copy_into(
             generated_plane,
-            {
-                let pos = self.space_info.position;
-
-                (pos.0 as u32 + 10, pos.1 as u32 + 25)
-            },
-            (200, 200),
+            pos.0 as u32 + 10,
+            pos.1 as u32 + 25,
+            200,
+            200,
+            ResizeMode::NearestNeighbor,
         )?;
 
         Ok(())
